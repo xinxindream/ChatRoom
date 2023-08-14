2941281726ec70bc73d5a6ddeb2032f9223175e5
xinxindream <1877329715@qq.com>
2023-08-14 17:56:24 +0800

    增加服务器数据协议配置

---
+++ Server/config.py
@@ -0,0 +1,11 @@
+"""
+@Description: 服务器数据协议相关配置
+@Author: Pan Xiaofei
+@Date: 2023-08-14 17:52:46
+"""
+
+REQUEST_LOGIN = '0001'  # 登录请求
+REQUEST_CHAT = '0002'   # 聊天请求
+RESPONSE_LOGIN_RESULT = '1001'  # 登录结果响应
+RESPONSE_CHAT = '1002'  # 聊天响应
+DELIMITER = '|'     # 自定义协议数据分隔符
