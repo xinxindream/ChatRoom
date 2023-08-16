5ea9de538b5eb05b8d8d1442518adb388d79ca08
xinxindream <1877329715@qq.com>
2023-08-16 16:28:43 +0800

    v0.3,实现服务器多客户端连接以及多次交互

--- Server/server.py
+++ Server/server.py
@@ -23,23 +23,27 @@ class Server(object):
     """
     def startup(self):
         # 获取客户端连接
-        print("正在获取客户端连接~~~")
-        client_socket, client_addr = self.server_socket.accept()
-        print("已建立连接~~~")
-        
-        # 收发信息，引入封装套接字
-        ## 创建套接字对象
-        my_client_socket = mySocketWrapper(client_socket)
-        
-        ## 收，解码
-        print(my_client_socket.recvData())
+        # 持续获取客户端连接（可获取多个客户端连接）
+        while True:
+            print("正在获取客户端连接~~~")
+            client_socket, client_addr = self.server_socket.accept()
+            print("已建立连接~~~")
+            
+            # 允许客户端与服务器多次交互
+            while True: 
+                # 收发信息，引入封装套接字
+                ## 创建套接字对象
+                my_client_socket = mySocketWrapper(client_socket)
+                
+                ## 收，解码
+                print(my_client_socket.recvData())
 
-        ## 发，编码
-        msg = "你好！"
-        my_client_socket.sendData(msg)
+                ## 发，编码
+                msg = "你好！"
+                my_client_socket.sendData(msg)
 
-        client_socket.close()
-        self.server_socket.close()
+            client_socket.close()
+            self.server_socket.close()
 
 
 if __name__ == "__main__":
