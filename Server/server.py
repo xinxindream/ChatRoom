b118fe1f621b4daa2abbba91bc177434a7207040
xinxindream <1877329715@qq.com>
2023-08-17 17:18:43 +0800

    增加客户端断开连接

--- Server/server.py
+++ Server/server.py
@@ -18,15 +18,42 @@ class Server(object):
         self.server_socket = myServerSocket()
 
     """
+    @Description: 解析客户端请求
+    @Parameters: 
+    @Return: 
+    """
+    def parseRequest(self, recv_data):
+        parse_data_list = recv_data.split('|')
+
+    """
+    @Description: 客户端主动关闭连接
+    @Parameters: 客户端地址
+    @Return: 
+    """
+    def logOut(self, client_addr):
+        print(f"用户{client_addr[0]}:{client_addr[1]}下线")
+
+    """
     @Description: 服务器处理客户端请求
     @Parameters: 通信套接字
     @Return: 
     """
-    def requestHandler(self, my_client_socket):
+    def requestHandler(self, my_client_socket, client_addr):
         # 允许客户端与服务器多次交互
         while True: 
             ## 收，解码
-            print(my_client_socket.recvData())
+            recv_data = my_client_socket.recvData()
+
+            ## 若客户端发送信息为空，则判断下线，关闭客户端套接字，但保持服务器套接字开启
+            if not recv_data:
+                self.logOut(client_addr)
+                my_client_socket.closeSocket()
+                break
+
+            print("收到客户端信息：" + recv_data)
+
+            ## 解析数据
+            parse_data = self.parseRequest(recv_data)
 
             ## 发，编码
             msg = "你好！"
@@ -50,7 +77,7 @@ class Server(object):
             my_client_socket = mySocketWrapper(client_socket)
                 
             # 引入多线程，实现服务器与多个客户端同时交互
-            t = Thread(target=self.requestHandler, args=(my_client_socket,))
+            t = Thread(target=self.requestHandler, args=(my_client_socket, client_addr))
             t.start()
     
             # client_socket.close()
