a44fba06e2be4912ce599c5c0ae5a6e422729d8b
xinxindream <1877329715@qq.com>
2023-08-19 17:54:40 +0800

    更新数据库配置

---
+++ DB/properties.py
@@ -0,0 +1,7 @@
+""" 数据库基本信息配置 """
+DB_HOST = '127.0.0.1' # mysql服务器连接端口
+DB_PORT = 3306    # mysql服务器端口
+DB_USER = 'root'    # mysql用户名
+DB_PASSWORD = '010908'  # 用户密码
+DB_DATABASE = 'chatroom'    # 所需连接数据库名
+DB_CHARSET = 'utf8' # 编码格式
